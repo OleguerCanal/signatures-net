10d1aa3753652683c5b0be7285495dfefb421a15
OleguerCanal <oleguer.canal@hotmail.com>
2024-08-11 22:25:25 +0200

    Tested aute-encoder version of classifier (non-variational)

--- signaturesnet/trainers/vae_classifier_trainer.py
+++ signaturesnet/trainers/vae_classifier_trainer.py
@@ -50,11 +50,12 @@ class VaeClassifierTrainer:
             device=device)
 
 
-    def __loss(self, input, pred, z_mu, z_std):
-        kl_div = (0.5*(z_std.pow(2) + z_mu.pow(2) - 2*torch.log(z_std) - 1).sum(dim=1)).mean(dim=0)
-        reconstruction = nn.MSELoss()(input, pred)
-        # reconstruction = get_jensen_shannon(predicted_label=pred, true_label=input)
-        return reconstruction + self.adapted_lagrange_param*kl_div
+    def __loss(self, inputs, pred, z_mu, z_std):
+        # kl_div = (0.5*(z_std.pow(2) + z_mu.pow(2) - 2*torch.log(z_std) - 1).sum(dim=1)).mean(dim=0)
+        # reconstruction = nn.MSELoss()(inputs, pred)
+        # # # reconstruction = get_jensen_shannon(predicted_label=pred, true_label=inputs)
+        # return reconstruction + self.adapted_lagrange_param*kl_div
+        return nn.MSELoss()(inputs, pred)
 
     def objective(self,
                   batch_size,
@@ -92,6 +93,7 @@ class VaeClassifierTrainer:
             {'params': model.encoder_layers.parameters(), 'lr': lr_encoder},
             {'params': model.decoder_layers.parameters(), 'lr': lr_decoder}
         ])
+        model.train()
 
         # l_vals = collections.deque(maxlen=50)
         # max_found = -np.inf
@@ -104,9 +106,8 @@ class VaeClassifierTrainer:
         for iteration in range(self.iterations):
             for train_input, train_labels, _, train_nummut, _ in tqdm(dataloader):
                 # train_input = train_input[(train_labels == 1).squeeze(-1)]
-                model.train()
                 optimizer.zero_grad()
-                train_pred, train_mean, train_std = model(train_input, train_nummut)
+                train_pred, train_mean, train_std = model(train_input, train_nummut, noise=False)
                 self.adapted_lagrange_param = self.lagrange_param
                 # if step < total_steps*0.8:
                 #     self.adapted_lagrange_param = self.lagrange_param * \
@@ -115,7 +116,7 @@ class VaeClassifierTrainer:
                 #     self.adapted_lagrange_param = self.lagrange_param * \
                 #         float(total_steps - total_steps*0.8)/float(total_steps)
                 train_loss = self.__loss(
-                    input=train_input,
+                    inputs=train_input,
                     pred=train_pred,
                     z_mu=train_mean,
                     z_std=train_std,
@@ -124,20 +125,26 @@ class VaeClassifierTrainer:
                 train_loss.backward()
                 optimizer.step()
 
-                model.eval()
-                with torch.no_grad():
-                    # val_inputs = self.val_dataset.inputs[(self.val_dataset.labels == 1).squeeze(-1)]
-                    val_inputs = self.val_dataset.inputs
-                    val_pred, val_mean, val_std = model(
-                        val_inputs, self.val_dataset.num_mut)
-                    val_loss = self.__loss(input=val_inputs,
-                                           pred=val_pred,
-                                           z_mu=val_mean,
-                                           z_std=val_std)
                     # l_vals.append(val_loss.item())
                     # max_found = max(max_found, -np.nanmean(l_vals))
 
                 if plot and step % self.log_freq == 0:
+                    model.eval()
+                    with torch.no_grad():
+                        # val_inputs = self.val_dataset.inputs[(self.val_dataset.labels == 1).squeeze(-1)]
+                        val_inputs = self.val_dataset.inputs
+                        val_pred, val_mean, val_std = model(
+                            val_inputs,
+                            self.val_dataset.num_mut,
+                            noise=False
+                        )
+                        val_loss = self.__loss(
+                            inputs=val_inputs,
+                            pred=val_pred,
+                            z_mu=val_mean,
+                            z_std=val_std
+                        )
+    
                     print('Logger...')
                     current_train_DQ99R = self.logger.log(
                         train_loss=train_loss,
@@ -154,8 +161,11 @@ class VaeClassifierTrainer:
                         model=model,
                     )
                     train_DQ99R = current_train_DQ99R if current_train_DQ99R is not None else train_DQ99R
+                    
+                    model.train()
+                    
 
-                if self.model_path is not None and step % 500 == 0:
+                if self.model_path is not None and step % 1000 == 0:
                     print('Saving model...')
                     save_model(model=model, directory=self.model_path)
                 step += 1
